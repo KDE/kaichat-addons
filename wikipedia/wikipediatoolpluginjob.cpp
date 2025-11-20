26f63a0d00fc79b6f45a13995449345426c6ab62
Laurent Montel <montel@kde.org>
2025-11-20 07:23:41 +0100

    Prepare to implement wikipediatoolpluginjob

--- wikipedia/wikipediatoolpluginjob.cpp
+++ wikipedia/wikipediatoolpluginjob.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "wikipediatoolpluginjob.h"
+#include "wikipedia_tool_debug.h"
 #include <QDebug>
 using namespace Qt::Literals::StringLiterals;
 WikipediaToolPluginJob::WikipediaToolPluginJob(QObject *parent)
@@ -22,6 +23,35 @@ void WikipediaToolPluginJob::start()
         deleteLater();
         return;
     }
+
+    Q_EMIT toolInProgress(i18n("Get Wikipedia Page."));
+    QString title;
+    const QStringList lst = requiredArguments();
+    for (const auto &arg : lst) {
+        for (const auto &resultTool : std::as_const(mToolArguments)) {
+            if (resultTool.keyTool == arg) {
+                const QString value = resultTool.value;
+                if (arg == "title"_L1) {
+                    title = value;
+                    qCDebug(WIKIPEDIA_TOOL_LOG) << "title: " << title;
+                }
+            }
+        }
+    }
+
+    if (title.isEmpty()) {
+        const TextAutoGenerateText::TextAutoGenerateTextToolPlugin::TextToolPluginInfo info{
+            .content = i18n("No title found"),
+            .messageUuid = mMessageUuid,
+            .chatId = mChatId,
+            .toolIdentifier = mToolIdentifier,
+            .attachementInfoList = {},
+        };
+        Q_EMIT finished(info);
+        deleteLater();
+        return;
+    }
+
     // qDebug() << " TextAutoGenerateText::TextAutoGenerateTextToolPlugin::TextToolPluginInfo " << info;
     Q_EMIT finished({});
     deleteLater();
